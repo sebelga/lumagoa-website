3d8ef71f89e8785dd99b54d1971bce558150e55c
Sébastien Loix <seb@Mac.Dlink>
2025-08-03 17:05:22 +0200

    Use templates for header and footer

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,18 +1,41 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { createMpaPlugin } from "vite-plugin-virtual-mpa";
 
 export default defineConfig({
-  plugins: [tailwindcss({ config: "./tailwind.config.js" })],
+  plugins: [
+    {
+      name: "redirect-root", // Custom plugin for dev server redirect
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          if (req.url === "/") {
+            req.url = "/index.html"; // Required for the Mpa plugin to work correctly
+          }
+          next();
+        });
+      },
+    },
+    createMpaPlugin({
+      pages: [
+        {
+          name: "index",
+          template: resolve(__dirname, "src/pages/index.ejs"),
+          filename: "index.html",
+        },
+        {
+          name: "termsAndServices",
+          template: resolve(__dirname, "src/pages/terms-and-services.ejs"),
+          filename: "terms-and-services.html",
+        },
+      ],
+      htmlMinify: true,
+    }),
+    tailwindcss({ config: "./tailwind.config.js" }),
+  ],
   // Basic config for static site
   build: {
     outDir: "dist", // Output directory for builds
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        termsAndServices: resolve(__dirname, "terms-and-services.html"),
-      },
-    },
   },
   server: {
     open: true, // Auto-open browser on dev server start
